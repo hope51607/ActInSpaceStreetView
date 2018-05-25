38370a87b7b854b5aacb9f9daeda972ba355e6fb
hope51607 <hope51607@yahoo.com.tw>
2018-05-25 20:15:24 +0800

    console.log

--- index.js
+++ index.js
@@ -13,16 +13,16 @@ $(document).ready(function(){
     switch(error.code) 
         {
         case error.PERMISSION_DENIED:
-        x.innerHTML="User denied the request for Geolocation."
+        console.log("User denied the request for Geolocation.")
         break;
         case error.POSITION_UNAVAILABLE:
-        x.innerHTML="Location information is unavailable."
+        console.log("Location information is unavailable.")
         break;
         case error.TIMEOUT:
-        x.innerHTML="The request to get user location timed out."
+        console.log("The request to get user location timed out.")
         break;
         case error.UNKNOWN_ERROR:
-        x.innerHTML="An unknown error occurred."
+        console.log("An unknown error occurred.")
         break;
         }
     }
